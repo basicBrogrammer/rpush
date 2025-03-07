2c99b523610166d2fcbc2d61db69ff555d79abe1
basicbrogrammer <jcward10@gmail.com>
2025-03-07 09:14:54 -0500

    Test fcm transform_values to_s for the data reader

--- lib/rpush/client/active_record/fcm/notification.rb
+++ lib/rpush/client/active_record/fcm/notification.rb
@@ -4,6 +4,10 @@ module Rpush
       module Fcm
         class Notification < Rpush::Client::ActiveRecord::Notification
           include Rpush::Client::ActiveModel::Fcm::Notification
+
+          def data
+            multi_json_load(read_attribute(:data)).transform_values(&:to_s) if read_attribute(:data)
+          end
         end
       end
     end
